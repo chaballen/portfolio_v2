fe0fd0cc9da0f73a5a30ede4c2f5fd9bac0a8ce3
chaballen <chab.a.allen@gmail.com>
2023-04-14 16:24:13 -0500

    Updates

---
+++ precache-manifest.02c30c1fb40ff745ef9e33494ac853ef.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "57fb0d2aa70dae715edd",
+    "url": "/static/css/main.ccbc973d.chunk.css"
+  },
+  {
+    "revision": "57fb0d2aa70dae715edd",
+    "url": "/static/js/main.63165682.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c1d6c2850095904ce673",
+    "url": "/static/js/2.37cbaf20.chunk.js"
+  },
+  {
+    "revision": "4271f8b01b2a4e1c24d8132387116617",
+    "url": "/static/media/background.4271f8b0.png"
+  },
+  {
+    "revision": "b3fc0f0d140100b208308655efc8cdea",
+    "url": "/static/media/Chabrielle_Allen_Resume.b3fc0f0d.pdf"
+  },
+  {
+    "revision": "061acee274cbe4089a5907525b09d1bf",
+    "url": "/static/media/headshot.061acee2.jpg"
+  },
+  {
+    "revision": "cbe57776b6cb4bc4c675900d84aaed34",
+    "url": "/static/media/cruise.cbe57776.jpg"
+  },
+  {
+    "revision": "0c8530337769593beaf256808a6ec8af",
+    "url": "/static/media/cncd.0c853033.jpg"
+  },
+  {
+    "revision": "750aa776a6a1c08b8354c5552c8ff224",
+    "url": "/static/media/Chabrielle_Allen_Productivity.750aa776.pdf"
+  },
+  {
+    "revision": "59ca8d634f1ee370962a8405cdb56fd7",
+    "url": "/static/media/Chabrielle_Allen_Empty_Self_Cults.59ca8d63.pdf"
+  },
+  {
+    "revision": "6c02a4d006d3de88e0bf11a234d40673",
+    "url": "/static/media/Chabrielle_Allen_Women_Crusades.6c02a4d0.pdf"
+  },
+  {
+    "revision": "ab7dc653a141d48da8f0bde79140cd79",
+    "url": "/static/media/Chabrielle_Allen_Violence_Revelation.ab7dc653.pdf"
+  },
+  {
+    "revision": "7b6f6823e61b954b987f5fcad7a49e84",
+    "url": "/static/media/Chabrielle_Allen_New_Beginning.7b6f6823.pdf"
+  },
+  {
+    "revision": "edc294f948922d27e8d44d677efb5ca5",
+    "url": "/static/media/self-care.edc294f9.jpg"
+  },
+  {
+    "revision": "f4720d5ddaae69e3e56b08f470d8a050",
+    "url": "/static/media/scuba.f4720d5d.jpg"
+  },
+  {
+    "revision": "76dcca28b90fe97a77cd45a2c2864c42",
+    "url": "/static/media/odd.76dcca28.jpg"
+  },
+  {
+    "revision": "3d38da2a1012fe27b4637652f838fcaf",
+    "url": "/static/media/seashells.3d38da2a.jpg"
+  },
+  {
+    "revision": "7b6787302b4e354bdcf8b1feec48e8cd",
+    "url": "/static/media/watercolor.7b678730.jpg"
+  },
+  {
+    "revision": "5b10971ab49fbc62c8b913ceb7d82e80",
+    "url": "/index.html"
+  }
+];
